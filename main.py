57fbeea70fd3a67e43dc4329886cc76a2f59db37
Ankit Dhingra <ankitsdhingra@gmail.com>
2015-01-19 13:58:41 -0500

    Basic Jinja Setup

--- main.py
+++ main.py
@@ -16,9 +16,32 @@
 #
 import webapp2
 
-class MainHandler(webapp2.RequestHandler):
+from webapp2_extras import jinja2
+
+# BaseHandler subclasses RequestHandler so that we can use jinja
+class BaseHandler(webapp2.RequestHandler):
+
+    @webapp2.cached_property
+    def jinja2(self):
+        # Returns a Jinja2 renderer cached in the app registry.
+        return jinja2.get_jinja2(app=self.app)
+
+        # This will call self.response.write using the specified template and context.
+        # The first argument should be a string naming the template file to be used.
+        # The second argument should be a pointer to an array of context variables
+        #  that can be used for substitutions within the template
+    def render_response(self, _template, **context):
+        # Renders a template and writes the result to the response.
+        rv = self.jinja2.render_template(_template, **context)
+        self.response.write(rv)
+
+class MainHandler((BaseHandler)):
     def get(self):
-        self.response.write('Hello world!')
+         # this will eventually contain information about the RSS feed
+        context = {}
+
+                  # here we call render_response instead of self.response.write.
+        self.render_response('index.html', **context)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
